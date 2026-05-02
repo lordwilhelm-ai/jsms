60502f3b2634639eb08de1e0bd8b6a1025dd8531
Lord Wilhelm <mensahpaa@gmail.com>
2026-05-02 07:31:49 +0000

    Fix empty duty roster page

---
+++ app/(protected)/teacher-attendance/duty-roster/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function DutyRosterPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Duty Roster</h1>
+      <p>This page is coming soon.</p>
+    </main>
+  );
+}
